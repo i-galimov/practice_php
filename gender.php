a86e574d7831aeb883030dfb81d0dab4422ccc85
Ilshat Galimov <86232244+ilshat1kzn@users.noreply.github.com>
2021-07-24 14:59:24 +0300

    Tags
    Add tags

--- gender.php
+++ gender.php
@@ -1,8 +1,11 @@
 // напечатать "Male", если переменная "gender" равна 0, "Female", если она равна 1, и "Undefined" во всех остальных случаях.
 
+<?php
 if ($gender == 0)
    echo "Male";
 elseif ($gender == 1)
    echo "Female";
 else 
    echo "Undefined";
+?>
+
