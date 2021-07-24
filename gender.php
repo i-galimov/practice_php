4c3dde124b99d15169e4c69c85d2156bd8c82aac
Ilshat Galimov <86232244+ilshat1kzn@users.noreply.github.com>
2021-07-24 14:54:31 +0300

    create gender.php
    Adda new file

---
+++ gender.php
@@ -0,0 +1,8 @@
+// напечатать "Male", если переменная "gender" равна 0, "Female", если она равна 1, и "Undefined" во всех остальных случаях.
+
+if ($gender == 0)
+   echo "Male";
+elseif ($gender == 1)
+   echo "Female";
+else 
+   echo "Undefined";
